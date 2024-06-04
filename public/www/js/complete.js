0f705b658f8d7ac83cb50945ecce1de85c28339a
shparknr11 <shparknr11@naver.com>
2024-06-04 17:04:42 +0900

    docs : complete.html 퍼블리싱 진행 중

---
+++ public/www/js/complete.js
@@ -0,0 +1,12 @@
+// window.addEventListener("load", function () {
+//   const checkbox = document.querySelector(".com-checkbox");
+//   const hoverText = document.querySelector(".hover-area");
+
+//   checkbox.addEventListener("change", function () {
+//     if (checkbox.checked) {
+//       hoverText.style.backgroundColor = "";
+//     } else {
+//       hoverText.style.backgroundColor = null;
+//     }
+//   });
+// });
