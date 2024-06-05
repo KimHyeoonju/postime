668853b98a470f38650dac7f5f802aad0956bf27
shparknr11 <shparknr11@naver.com>
2024-06-05 16:32:20 +0900

    docs : home-header, serach, complete 페이지 퍼블리싱 이후 기능추가 진행중

--- public/www/js/header.js
+++ public/www/js/header.js
@@ -1,20 +1,24 @@
-const headerHtml = `
-<div class="header-inner">
-              <ul class="header-list">
-                <li class="user-name">{username}<a href="#">더보기</a></li>
-                <li class="search-box">
-                  <input class="search-text" type="text" autocomplete="off" />
-                </li>
-                <li class="search"><a href="#"></a></li>
-                <li class="menu"><a href="#"></a></li>
-              </ul>
-            </div>
+const headerHtml = ` <div class="header-inner">
+<ul class="header-list">
+  <li class="header-user-name">{username}<a href="#">V</a></li>
+  <li class="search-box">
+    <input class="search-text" type="text" autocomplete="off" />
+  </li>
+  <li class="header-search"><a href="#"></a></li>
+  <li class="header-menu"><a href="#"></a></li>
+</ul>
+<div class="header-more">
+  <div class="header-my">마이페이지</div>
+  <div class="header-logout">로그아웃</div>
+</div>
+</div>
 `;
 
 const headerClass = document.querySelector(".header");
 
 headerClass.innerHTML = headerHtml;
 
+// 검색 박스
 // window.addEventListener("load", function () {
 //   const search = document.querySelector(".icon-search");
 //   const searchBox = document.querySelector(".header-search-box");
@@ -33,3 +37,24 @@ headerClass.innerHTML = headerHtml;
 //     }
 //   });
 // });
+
+// 마이페이지로그인 메뉴
+window.addEventListener("load", function () {
+  const moreBt = document.querySelector(".header-user-name a");
+  const moreTag = document.querySelector(".header-more");
+  // console.log(moreBt);
+  // console.log(moreTag);
+  let moreOpen = false;
+  moreBt.addEventListener("click", function () {
+    // console.log("클릭됐어");
+    if (moreOpen) {
+      moreTag.classList.remove("header-more-open");
+      moreOpen = false;
+      // console.log(moreOpen);
+    } else {
+      moreTag.classList.add("header-more-open");
+      moreOpen = true;
+      // console.log(moreOpen);
+    }
+  });
+});
--- public/www/js/todolistMenuBtnClick.js
+++ public/www/js/todolistMenuBtnClick.js
@@ -14,3 +14,18 @@ todoMenuBtn.addEventListener("click", () => {
   }
   console.log(todolistClick.className);
 });
+
+// window.addEventListener("load", function () {
+//   const todoMenuBtn = document.querySelector(".header-menu");
+//   const todolistClick = document.querySelector(".todo-list-menu-wrap");
+//   let listOpen = false;
+//   todoMenuBtn.addEventListener("click", function () {
+//     if (listOpen) {
+//       todolistClick.classList.remove("todolistOff");
+//       listOpen = false;
+//     } else {
+//       todolistClick.classList.add("todolistOff");
+//       listOpen = true;
+//     }
+//   });
+// });
