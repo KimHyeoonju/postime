aecb5f0321b8b453f5e48540e4beb99a6d9ade36
suminkim8203 <kimsm8203@gmail.com>
2024-06-11 14:43:09 +0900

    글쓰기 페이지 form 태그 추가 후 레이아웃 정리

--- src/pages/write/Create.js
+++ src/pages/write/Create.js
@@ -22,13 +22,50 @@ const Create = () => {
   const [createWrite, setCreateWrite] = useState("");
   const [createDate, setCreateDate] = useState();
 
-  const 
+  // 댓글 관련
+  const [commentInput, setCommentInput] = useState("");
+
+  const createPost = async event => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post("/api/board", {
+        calendarId: "calendarId",
+        signedUserId: "signedUserId",
+        title: "title",
+        content: "content",
+        startDay: "startDay",
+        deadLine: "deadLine",
+        existTag: [1, 2],
+        notExistTag: [
+          {
+            calendarId: "calendarId",
+            title: "tag1",
+            color: 1,
+          },
+        ],
+        dDay: "dDay",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleTitleChange = event => {
+    setCreateTitle(event.target.value);
+    // console.log("Title:", event.target.value);
+  };
+
+  const handleWriteChange = event => {
+    setCreateWrite(event.target.value);
+  };
+
+  const handleCommentChange = event => {
+    setCommentInput(event.target.value);
+  };
 
   const fileUpload = () => {
     const imgUploadBt = document.querySelector(".img-upload-button");
-    // const imgUpload = document.querySelector(".img-upload");
-
-    // imgUpload.addEventListener("click", () => imgUploadBt.click());
     imgUploadBt.click();
   };
 
@@ -85,36 +122,18 @@ const Create = () => {
   return (
     <div className="write-wrap">
       <div className="write-inner">
-        {/* 글쓰기 상단 제목부 */}
-        <div className="write-header">
-          <div className="write-button">
-            <button className="write-button-primary">
-              <span>저장</span>
-            </button>
-            <button className="write-button-primary">
-              <span>삭제</span>
-            </button>
-          </div>
-          <div className="write-header-text">
-            {/* <form>
-              <textarea
-                type="text"
-                id="write-header-title"
-                placeholder="제목 없음"
-                maxLength="28"
-                name="title"
-                onChange={event => {
-                  setCreateTitle(event.target.value);
-                }}
-              ></textarea>
-            </form> */}
-            <form
-              className="write-header-title"
-              action="/detail"
-              method="POST"
-              // onSubmit={handleWriteChange}
-              // onChange={setCreateTitle}
-            >
+        <form className="write-header-title">
+          {/* 글쓰기 상단 제목부 */}
+          <div className="write-header">
+            <div className="write-button">
+              <button className="write-button-primary">
+                <span>저장</span>
+              </button>
+              <button className="write-button-primary">
+                <span>삭제</span>
+              </button>
+            </div>
+            <div className="write-header-text">
               <textarea
                 id="write-header-title"
                 type="text"
@@ -122,18 +141,17 @@ const Create = () => {
                 maxLength="28"
                 name="title"
                 value={createTitle}
+                onChange={handleTitleChange}
               ></textarea>
-            </form>
 
-            <div className="write-header-dec">
-              <span>
-                <IoBookmarkSharp /> 내 캘린더
-              </span>
-              <div className="write-header-info">
-                <div className="write-header-icon">
-                  <FaRegCalendar /> 2024.06.03 - 2024.06.07
-                </div>
-                <div className="write-header-icon">
+              <div className="write-header-dec">
+                <span>
+                  <IoBookmarkSharp /> 내 캘린더
+                </span>
+                <div className="write-header-info">
+                  <div className="write-header-icon">
+                    <FaRegCalendar /> 2024.06.03 - 2024.06.07
+                  </div>
                   <input
                     name="tags"
                     className="write-tags"
@@ -144,86 +162,95 @@ const Create = () => {
               </div>
             </div>
           </div>
-        </div>
-        {/* 글쓰기 본문 */}
-        <div className="write-main">
-          <div className="write-main-text">
-            <textarea
-              type="text"
-              placeholder="내용을 입력하세요."
-              // value={createWrite}
-              // onChange={handleWriteChange}
-            ></textarea>
-          </div>
-          {/* 이미지 업로드 부분 */}
-          <div className="write-img">
-            <div className="write-img-warp">
-              <div className="write-img-inner">
-                <button className="img-upload" onClick={fileUpload}>
-                  <label htmlFor="input-file" onChange={handleImgUpload}>
-                    <span>이미지 업로드</span>
-                    <input
-                      type="file"
-                      className="img-upload-button"
-                      accept="image/*"
-                      required
-                      multiple
-                    />
-                  </label>
-                </button>
+          {/* 글쓰기 본문 */}
+          <div className="write-main">
+            <div className="write-main-text">
+              <textarea
+                type="text"
+                placeholder="내용을 입력하세요."
+                value={createWrite}
+                onChange={handleWriteChange}
+              ></textarea>
+            </div>
+            {/* 이미지 업로드 부분 */}
+            <div className="write-img">
+              <div className="write-img-warp">
+                <div className="write-img-inner">
+                  <button
+                    className="img-upload"
+                    onClick={fileUpload}
+                    type="button"
+                  >
+                    <label htmlFor="input-file" onChange={handleImgUpload}>
+                      <span>이미지 업로드</span>
+                      <input
+                        type="file"
+                        className="img-upload-button"
+                        accept="image/*"
+                        required
+                        multiple
+                      />
+                    </label>
+                  </button>
 
-                <div>
-                  {/* 저장해둔 이미지들을 순회하면서 화면에 이미지 출력 */}
-                  {imgFile.map((image, id) => (
-                    <div key={id}>
-                      {/* <IoMdClose
+                  <div>
+                    {/* 저장해둔 이미지들을 순회하면서 화면에 이미지 출력 */}
+                    {imgFile.map((image, id) => (
+                      <div key={id}>
+                        {/* <IoMdClose
                         onClick={() => handleDeleteImage(id)}
                         className="img-delete"
                       /> */}
-                      <img
-                        src={image}
-                        alt={`${image}-${id}`}
-                        className="write-img-contain"
-                      />
-                    </div>
-                  ))}
-                </div>
-                {/* <div className="write-img-contain" id="write-img-show">
+                        <img
+                          src={image}
+                          alt={`${image}-${id}`}
+                          className="write-img-contain"
+                        />
+                      </div>
+                    ))}
+                  </div>
+                  {/* <div className="write-img-contain" id="write-img-show">
                   {imgFile?.map((img, idx) => (
                     <div key={idx}>
                       <img src={img} alt="img" className="write-img-contain" />
                     </div>
                   ))}
                 </div> */}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      {/* 메인 우측 댓글 */}
-      <div className="chat-wrap">
-        <div className="chat-inner">
-          <div className="chat-header">
-            <h2>댓글</h2>
-          </div>
-          <div className="chat-comment">
-            <div className="chat-comment-inner">
-              <div className="chat-user-text">
-                <div className="chat-user">수민</div>
-                <div className="user-comment">부탁드립니다.</div>
+        </form>
+        <form className="chat-wrap">
+          {/* 메인 우측 댓글 */}
+
+          <div className="chat-header">댓글</div>
+          <div className="chat-inner">
+            <div className="chat-comment">
+              <div className="chat-comment-inner">
+                <div className="chat-user-text">
+                  <div className="chat-user">수민</div>
+                  <div className="user-comment">부탁드립니다.</div>
+                </div>
               </div>
-              <i className="xi-minus-circle-o xi-2x"></i>
+              <div className="chat-comment-inner">
+                <div className="chat-user-text">
+                  <div className="chat-user">작성자</div>
+                  <div className="user-comment">{handleCommentChange}</div>
+                </div>
+              </div>
             </div>
             <div className="chat-comment-write">
               <input
                 type="text"
                 placeholder="댓글을 작성하세요."
                 className="chat-comment-input"
+                value={commentInput}
+                onChange={handleCommentChange}
               />
-              <i className="xi-location-arrow xi-2x"></i>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
